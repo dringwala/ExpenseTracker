5972aee88539dd71ae01bcee7497ecec89a48f44
Deval Ringwala <ringwala@lucasware.com>
2018-08-18 14:32:25 -0400

    Initial commit to add a project.

---
+++ MVC4/ExpenseTracker/Models/Bank.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExpenseTracker.Models
+{
+    public class Bank
+    {
+        public Guid BankId { get; set; }
+        public string Name { get; set; }
+        public string FriendlyName { get; set; }
+        public Guid BankUserId { get; set; }
+
+    }
+}
---
+++ MVC4/ExpenseTracker/Models/ExpenseTrackerContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ExpenseTracker.Models
+{
+    public class ExpenseTrackerContext : DbContext
+    {
+        public DbSet<Bank> Banks { get; set; }
+        public DbSet<BankAccount> Accounts { get; set; }
+    }
+}
